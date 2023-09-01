d528c817d22566a8e154ec2a79f3a05dc6d96b57
sangarusateesh <sangaru.satti@gmail.com>
2023-09-01 11:29:52 +0530

    changes for understanding the git commit -a -m message.

--- hello.php
+++ hello.php
@@ -1,3 +1,4 @@
 <?php
 print_r($_SERVER['REMOTE_ADDR']);die;
+
 ?>
--- logout.php
+++ logout.php
@@ -1,1 +1,5 @@
-<?phpinclude 'includes/conf.php';if (session_status() == PHP_SESSION_NONE) { session_start(); }unset($_SESSION[SESSION_VAR]['user']);header('Location: '.SITE_URL."/login.php");exit;
+<?php
+    include 'includes/conf.php';
+    if (session_status() == PHP_SESSION_NONE) { session_start(); }
+    unset($_SESSION[SESSION_VAR]['user']);
+    header('Location: '.SITE_URL."/login.php");exit;
